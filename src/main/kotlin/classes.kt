91d820eb51961c9efc93c396bde0e70275eb21dc
nadiaroschina <nadysha.rr@gmail.com>
2021-11-21 01:24:40 +0300

    introduced classes Group, Rank & data classes

---
+++ src/main/kotlin/classes.kt
@@ -0,0 +1,25 @@
+import java.time.LocalDateTime
+
+class Group(groupName: String) {
+    init {
+        require(groups.contains(groupName)) { "group name must be mentioned in config file" }
+    }
+}
+
+class Rank(rankName: String) {
+    init {
+        require(ranks.contains(rankName)) { "rank name must be mentioned in config file" }
+    }
+}
+
+data class Athlete(
+    val surname: String, val name: String, val birthYear: Int,
+    val group: Group, val rank: Rank,
+    val athleteNumber: Int, val startTime: LocalDateTime,
+    val teamName: String
+)
+
+data class Team(val teamName: String, val athletes: List<Athlete>)
+
+data class AthletesGroup(val group: Group, val athletes: List<Athlete>)
+
