489b60cb29f771c4be64d2c572d72d47a0453be9
nadiaroschina <nadysha.rr@gmail.com>
2021-11-23 01:23:30 +0300

    config data structured

--- src/main/kotlin/ru/emkn/kotlin/sms/init.kt
+++ src/main/kotlin/ru/emkn/kotlin/sms/init.kt
@@ -6,16 +6,23 @@ data class GroupData(
     val group: String,
     val distance: String
 )
+
 data class CriteriaData(
     val distance: String,
     val checkpoints: List<String>
 )
+
 data class ConfigData(
     val eventName: String, val eventDate: String, val eventSport: String,
     val ranks: List<String>,
     val groups: List<GroupData>,
-    val criteria: List<CriteriaData>)
+    val criteria: List<CriteriaData>
+)
 
 val config = ConfigLoader().loadConfigOrThrow<ConfigData>("/config.yaml")
+
 val RANKS = config.ranks
 val GROUP_NAMES = config.groups.map { it.group }
+val GROUP_DISTANCES = config.groups.associate { groupData -> Pair(groupData.group, groupData.distance) }
+val DISTANCE_CRITERIA =
+    config.criteria.associate { criteriaData -> Pair(criteriaData.distance, criteriaData.checkpoints) }
