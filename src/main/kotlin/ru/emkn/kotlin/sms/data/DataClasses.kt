bdb92557e719a5b57f35641832c85f6136efcf2c
silvarum <alex26.std@gmail.com>
2021-11-25 02:30:46 +0300

    edit of data class, generationResultsTeam introduced

--- src/main/kotlin/ru/emkn/kotlin/sms/data/DataClasses.kt
+++ src/main/kotlin/ru/emkn/kotlin/sms/data/DataClasses.kt
@@ -48,8 +48,8 @@ data class AthletesGroup(val group: Group, val athletes: List<Athlete>)
 
 // for the protocolResults
 data class MedalTable(val athleteNumberInGroup: Int, val athleteNumber: Int,
-                      val suranme: String, val name: String, val birthYear: Int,
-                      val rank: Rank, val teamName: String, val result: LocalDateTime?,
-                      val place: Int, val backlog: String? = null)
+                      val surname: String, val name: String, val birthYear: Int,
+                      val rank: Rank?, val teamName: String, val result: LocalDateTime?,
+                      val place: Int?, val backlog: String)
 
 data class AthleteResults(val group: Group, val resultsPerGroup: List<MedalTable>)
