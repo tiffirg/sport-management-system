57b941b50a6b42644a01d30514e41ce38407ace3
nadiaroschina <nadysha.rr@gmail.com>
2021-11-26 21:49:36 +0300

    startProtocolGeneration made an internal function

--- src/main/kotlin/ru/emkn/kotlin/sms/App.kt
+++ src/main/kotlin/ru/emkn/kotlin/sms/App.kt
@@ -37,7 +37,7 @@ object App {
         CsvHandler.generationProtocolsStart(pathProtocolStart, startLists)
     }
 
-    private fun startProtocolsGeneration(applications: List<Team>): List<AthletesGroup> {
+    internal fun startProtocolsGeneration(applications: List<Team>): List<AthletesGroup> {
 
         // формирование списков участников по группам
         val groupLists: Map<Group, MutableList<Athlete>> =
