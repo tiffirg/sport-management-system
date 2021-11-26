e0689ed3d34549bd80be4e76804dedb33050abdf
nadiaroschina <nadysha.rr@gmail.com>
2021-11-26 22:17:28 +0300

    TimeFormatter.kt implemented

---
+++ src/main/kotlin/ru/emkn/kotlin/sms/classes/TimeFormatter.kt
@@ -0,0 +1,14 @@
+package ru.emkn.kotlin.sms.classes
+import java.time.format.DateTimeFormatter
+
+val TimeFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("hh:mm:ss")
+val DateFormat : DateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy")
+
+
+/*
+usage example:
+val sampleDateTime = LocalDateTime.of(LocalDate.parse("2003-09-22"), LocalTime.parse("12:01:18"))
+println(sampleDateTime)    // 2003-09-22T12:01:18
+println(sampleDateTime.format(TimeFormatter))   // 12:01:18
+println(sampleDateTime.format(DateFormat))  // 22.09.2003
+ */
