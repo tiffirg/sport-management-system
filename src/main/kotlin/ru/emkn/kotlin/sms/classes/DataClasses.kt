442d903a37baa458915a6c48965dc06f2512150a
nadiaroschina <nadysha.rr@gmail.com>
2021-11-27 11:10:28 +0300

    data class medalTable divided to ProtocolString and ProtocolGroup

--- src/main/kotlin/ru/emkn/kotlin/sms/classes/DataClasses.kt
+++ src/main/kotlin/ru/emkn/kotlin/sms/classes/DataClasses.kt
@@ -7,9 +7,10 @@ data class Team(val teamName: String, val athletes: List<Athlete>)
 data class AthletesGroup(val group: Group, val athletes: List<Athlete>)
 
 // for the protocolResults
-data class MedalTable(val athleteNumberInGroup: Int, val athleteNumber: Int,
+data class ProtocolString(val athleteNumberInGroup: Int, val athleteNumber: Int,
                       val surname: String, val name: String, val birthYear: Int,
                       val rank: Rank, val teamName: String, val result: LocalTime,
                       val place: Int, val backlog: String)
 
-data class AthleteResults(val group: Group, val resultsPerGroup: List<MedalTable>)
+data class ProtocolGroup (val group: Group, val protocols: List<ProtocolString>)
+
