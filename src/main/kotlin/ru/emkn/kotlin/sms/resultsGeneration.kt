b47dc35d9d594734147204cc730bc4d3b9fd26d9
nadiaroschina <nadysha.rr@gmail.com>
2021-11-27 21:48:44 +0300

    resultGeneration bug fixed: result is now absolute

--- src/main/kotlin/ru/emkn/kotlin/sms/resultsGeneration.kt
+++ src/main/kotlin/ru/emkn/kotlin/sms/resultsGeneration.kt
@@ -4,19 +4,49 @@ import ru.emkn.kotlin.sms.classes.*
 import java.time.LocalTime
 
 
-// TODO("проверить корректность прохождения дистанции участником? С помощью Athlete.CheckCheckpoints")
-fun getAthleteResult(athlete: Athlete): LocalTime? {
-    return if (athlete.checkpoints != null) {
-        athlete.checkpoints!!.last().time
+const val INF = 1000000
+
+fun LocalTime.minus(time: LocalTime?): LocalTime {
+    return if (time == null) {
+        this
     } else {
-        null
+        val totalSeconds = this.toSecondOfDay() - time.toSecondOfDay()
+        val h = totalSeconds / (60 * 60)
+        val m = totalSeconds / 60 - h * 60
+        val s = totalSeconds % 60
+        LocalTime.of(h, m, s)
     }
 }
 
-fun generateResultsGroup(athletesGroup: List<Athlete>): List<ResultAthleteGroup> {
+
+// TODO("проверить корректность прохождения дистанции участником? С помощью Athlete.CheckCheckpoints")
+fun getAthleteResult(athlete: Athlete): LocalTime? {
+
+    if (athlete.checkpoints == null) {
+        TODO("invalid athlete")
+    } else {
+
+        val distanceCriteria = DISTANCE_CRITERIA[athlete.group.distance]
+        val finish = distanceCriteria?.last() ?: throw Exception() // TODO: invalid distance criteria
+
+        return if (athlete.checkpoints != null) {
+            val dataCheckpoints = athlete.checkpoints!!.associate { Pair(it.checkpoint, it.time) }
+            dataCheckpoints[finish]?.minus(athlete.startTime) ?: throw Exception() // TODO: убрать вопросики
+        } else {
+            null
+        }
+    }
+}
+
+
+fun generateResultsGroup(athletesGroup: AthletesGroup): List<ResultAthleteGroup> {
     // TODO("добавить присвоение разрядов")
-    athletesGroup.sortedBy { athlete -> getAthleteResult(athlete) }
-    val protocols: List<ResultAthleteGroup> = athletesGroup.mapIndexed { index, athlete ->
+
+    val sortedAthletes = athletesGroup.athletes.sortedBy { athlete ->
+        getAthleteResult(athlete)?.toSecondOfDay() ?: INF
+    }
+
+    val protocols: List<ResultAthleteGroup> = sortedAthletes.mapIndexed { index, athlete ->
         ResultAthleteGroup(
             index + 1, athlete.athleteNumber!!,
             athlete.surname, athlete.name, athlete.birthYear,
@@ -31,7 +61,7 @@ fun generateResultsGroup(athletesGroup: List<Athlete>): List<ResultAthleteGroup>
 fun generateResults(dataCheckpoints: List<Athlete>): Map<Group, ResultsGroup> {
     val athletesGroups = dataCheckpoints.groupBy { athlete -> athlete.group }
     val protocols = (athletesGroups.map { (group, athletesGroup) ->
-        Pair(group, ResultsGroup(group, generateResultsGroup(athletesGroup)))
+        Pair(group, ResultsGroup(group, generateResultsGroup(AthletesGroup(group, athletesGroup))))
     }).toMap()
     return protocols
 }
