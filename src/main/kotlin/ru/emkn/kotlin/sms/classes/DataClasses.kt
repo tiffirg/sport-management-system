1676ad5b86def493a2cc61db0adefb41100df370
nadiaroschina <nadysha.rr@gmail.com>
2021-11-27 10:27:17 +0300

    MedalTable fields updated

--- src/main/kotlin/ru/emkn/kotlin/sms/classes/DataClasses.kt
+++ src/main/kotlin/ru/emkn/kotlin/sms/classes/DataClasses.kt
@@ -1,6 +1,6 @@
 package ru.emkn.kotlin.sms.classes
 
-import java.time.LocalDateTime
+import java.time.LocalTime
 
 data class Team(val teamName: String, val athletes: List<Athlete>)
 
@@ -9,7 +9,7 @@ data class AthletesGroup(val group: Group, val athletes: List<Athlete>)
 // for the protocolResults
 data class MedalTable(val athleteNumberInGroup: Int, val athleteNumber: Int,
                       val surname: String, val name: String, val birthYear: Int,
-                      val rank: Rank?, val teamName: String, val result: LocalDateTime?,
-                      val place: Int?, val backlog: String)
+                      val rank: Rank, val teamName: String, val result: LocalTime,
+                      val place: Int, val backlog: String)
 
 data class AthleteResults(val group: Group, val resultsPerGroup: List<MedalTable>)
