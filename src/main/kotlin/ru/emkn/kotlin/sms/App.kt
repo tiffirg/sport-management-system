d9e98b94789d918df31a71b094abdedec7128794
nadiaroschina <nadysha.rr@gmail.com>
2021-11-27 11:20:55 +0300

    startProtocolsGeneration extracted

--- src/main/kotlin/ru/emkn/kotlin/sms/App.kt
+++ src/main/kotlin/ru/emkn/kotlin/sms/App.kt
@@ -32,45 +32,9 @@ object App {
         }
         // генерирование стартовых списков
         val startLists: List<AthletesGroup> = startProtocolsGeneration(data)
-        // записывание данных в
+        // записывание данных в csv
         dir.mkdir()
         CsvHandler.generationProtocolsStart(pathProtocolStart, startLists)
-    }
-
-    internal fun startProtocolsGeneration(applications: List<Team>): List<AthletesGroup> {
-
-        // формирование списков участников по группам
-        val groupLists: Map<Group, MutableList<Athlete>> =
-            (applications.flatMap { team -> team.athletes }).groupByTo(mutableMapOf()) { athlete -> athlete.group }
-
-        // количество номеров, предусмотренных для участников из одной группы
-        val maxGroupSize = ((groupLists.maxOf { it.value.size } / 10 + 1) * 10)
-
-        // распределение времени старта между группами
-        fun generateStartTimes() {
-
-            var currentStartTime = EVENT_TIME.toLocalTime() ?: throw InvalidTimeException(EVENT_TIME) // TODO(Перенести в init)
-            var currentGroupIndex = 1
-
-            // жеребьевка внутри каждой группы
-            fun tossGroup(participants: MutableList<Athlete>) {
-                participants.shuffle()
-                participants.forEachIndexed { numberInGroup, athlete ->
-                    athlete.athleteNumber = currentGroupIndex * maxGroupSize + numberInGroup + 1
-                    athlete.startTime = currentStartTime
-                    currentStartTime = currentStartTime.plusMinutes(1)
-                }
-            }
-
-            groupLists.forEach { (_, participants) ->
-                tossGroup(participants)
-                currentGroupIndex++
-            }
-
-        }
-
-        generateStartTimes()
-        return groupLists.map { (group, athleteList) -> AthletesGroup(group, athleteList) }
     }
 
     private fun processCommandResultsGroup(command: CommandResultsGroup) {
