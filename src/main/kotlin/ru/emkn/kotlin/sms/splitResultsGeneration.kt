887dcc20d75f78c93b85fd8bfcca09d63e25ad20
nadiaroschina <nadysha.rr@gmail.com>
2021-11-27 22:02:11 +0300

    splitResultsGeneration template

---
+++ src/main/kotlin/ru/emkn/kotlin/sms/splitResultsGeneration.kt
@@ -0,0 +1,17 @@
+package ru.emkn.kotlin.sms
+import ru.emkn.kotlin.sms.classes.*
+import java.time.LocalTime
+
+fun getAthleteSplit(athlete: Athlete): List<LocalTime>? {
+    TODO()
+}
+
+
+fun generateSplitResultsGroup(athletesGroup: AthletesGroup): List<ResultAthleteGroup> {
+    TODO()
+}
+
+
+fun generateSplitResults(dataCheckpoints: List<Athlete>): Map<Group, Any> {
+    TODO()
+}
