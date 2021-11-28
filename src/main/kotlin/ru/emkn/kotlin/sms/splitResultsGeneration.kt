b74505d98327a6dda36a721faaa635c1f8e57512
nadiaroschina <nadysha.rr@gmail.com>
2021-11-28 17:02:53 +0300

    splits format changed to list of checkpointTime

--- src/main/kotlin/ru/emkn/kotlin/sms/splitResultsGeneration.kt
+++ src/main/kotlin/ru/emkn/kotlin/sms/splitResultsGeneration.kt
@@ -6,25 +6,28 @@ import java.time.LocalTime
 
 data class SplitResultAthleteGroup (val athleteNumberInGroup: Int, val athleteNumber: Int,
                                     val surname: String, val name: String, val birthYear: Int,
-                                    val rank: Rank, val teamName: String, val splits: List<LocalTime>?,
+                                    val rank: Rank, val teamName: String, val splits: List<CheckpointTime>?,
                                     val place: Int, val backlog: String)
 
 data class SplitResultsGroup(val group: Group, val results: List<SplitResultAthleteGroup>)
 
 
-fun getAthleteSplit(athlete: Athlete): List<LocalTime>? {
+fun getAthleteSplit(athlete: Athlete): List<CheckpointTime>? {
     athlete.checkCheckpoints()
     return if (athlete.removed) {
         null
     } else {
         // генерация сплитов: время на 1 КП - разница между временем отсечки и временем старта
         // время на последующие КП - разница времен текущего и предыдущего КП
-        val splits = mutableListOf<LocalTime>()
+        val splits = mutableListOf<CheckpointTime>()
         athlete.checkpoints!!.forEachIndexed { index, _ ->
             if (index == 0) {
-                splits.add(athlete.checkpoints!![0].time.minus(athlete.startTime))
+                val firstCheckpoint = athlete.checkpoints!![0]
+                splits.add(CheckpointTime(firstCheckpoint.checkpoint, firstCheckpoint.time.minus(athlete.startTime)))
             } else {
-                splits.add(athlete.checkpoints!![index].time.minus(athlete.checkpoints!![index - 1].time))
+                val currCheckpoint = athlete.checkpoints!![index]
+                val prevCheckpoint = athlete.checkpoints!![index - 1]
+                splits.add(CheckpointTime(currCheckpoint.checkpoint, currCheckpoint.time.minus(prevCheckpoint.time)))
             }
         }
         splits
