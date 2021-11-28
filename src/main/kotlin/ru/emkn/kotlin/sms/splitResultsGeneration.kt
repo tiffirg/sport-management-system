0819bd9b54e11987c803c6ddbdb065b03024d589
nadiaroschina <nadysha.rr@gmail.com>
2021-11-28 15:08:10 +0300

    splitResultsGeneration implemented

--- src/main/kotlin/ru/emkn/kotlin/sms/splitResultsGeneration.kt
+++ src/main/kotlin/ru/emkn/kotlin/sms/splitResultsGeneration.kt
@@ -4,22 +4,55 @@ import java.time.LocalTime
 
 
 
+data class SplitResultAthleteGroup (val athleteNumberInGroup: Int, val athleteNumber: Int,
+                                    val surname: String, val name: String, val birthYear: Int,
+                                    val rank: Rank, val teamName: String, val splits: List<LocalTime>?,
+                                    val place: Int, val backlog: String)
+
+data class SplitResultsGroup(val group: Group, val results: List<SplitResultAthleteGroup>)
+
+
 fun getAthleteSplit(athlete: Athlete): List<LocalTime>? {
-    require(athlete.checkpoints != null)
-    val orderedCheckpoints = DISTANCE_CRITERIA[athlete.group.distance]
-    val athleteData = athlete.checkpoints!!.associate { Pair(it.checkpoint, it.time) }
-    val orderedData = orderedCheckpoints?.associate { checkpoint -> Pair(checkpoint, athleteData[checkpoint]) }
-    val splits : List<LocalTime>
-
-    return null
+    athlete.checkCheckpoints()
+    return if (athlete.removed) {
+        null
+    } else {
+        // генерация сплитов: время на 1 КП - разница между временем отсечки и временем старта
+        // время на последующие КП - разница времен текущего и предыдущего КП
+        val splits = mutableListOf<LocalTime>()
+        athlete.checkpoints!!.forEachIndexed { index, _ ->
+            if (index == 0) {
+                splits.add(athlete.checkpoints!![0].time.minus(athlete.startTime))
+            } else {
+                splits.add(athlete.checkpoints!![index].time.minus(athlete.checkpoints!![index - 1].time))
+            }
+        }
+        splits
+    }
 }
 
 
-fun generateSplitResultsGroup(athletesGroup: AthletesGroup): List<ResultAthleteGroup> {
-    TODO()
+fun generateSplitResultsGroup(athletesGroup: AthletesGroup): List<SplitResultAthleteGroup> {
+    val sortedAthletes = athletesGroup.athletes.sortedBy { athlete ->
+        val resultTimeOrNull = getAthleteResult(athlete)
+        resultTimeOrNull?.toSecondOfDay() ?: INF
+    }
+    val splitProtocols: List<SplitResultAthleteGroup> = sortedAthletes.mapIndexed { index, athlete ->
+        SplitResultAthleteGroup(
+            index + 1, athlete.athleteNumber!!,
+            athlete.surname, athlete.name, athlete.birthYear,
+            athlete.rank, athlete.teamName, getAthleteSplit(athlete),
+            index + 1, ""
+        )
+    }
+    return splitProtocols
 }
 
 
-fun generateSplitResults(dataCheckpoints: List<Athlete>): Map<Group, Any> {
-    TODO()
+fun generateSplitResults(dataCheckpoints: List<Athlete>): Map<Group, SplitResultsGroup> {
+    val athletesGroups = dataCheckpoints.groupBy { athlete -> athlete.group }
+    val splitProtocols = (athletesGroups.map { (group, athletesGroup) ->
+        Pair(group, SplitResultsGroup(group, generateSplitResultsGroup(AthletesGroup(group, athletesGroup))))
+    }).toMap()
+    return splitProtocols
 }
