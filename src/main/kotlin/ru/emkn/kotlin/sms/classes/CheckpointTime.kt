26f96bda80fa9033e7ea002165bd928a61b4050c
nadiaroschina <nadysha.rr@gmail.com>
2021-12-14 00:11:19 +0300

    duration format refactored

--- src/main/kotlin/ru/emkn/kotlin/sms/classes/CheckpointTime.kt
+++ src/main/kotlin/ru/emkn/kotlin/sms/classes/CheckpointTime.kt
@@ -6,3 +6,15 @@ import java.time.LocalTime
 data class CheckpointTime(val checkpoint: String, val time: LocalTime)
 
 data class CheckpointDuration(val checkpoint: String, val duration: Duration)
+
+fun Duration?.toFormattedString() : String {
+    return if (this == null) {
+        ""
+    }
+    else {
+        val hours = this.toHoursPart().toString().padStart(2, '0')
+        val minutes = this.toMinutesPart().toString().padStart(2, '0')
+        val seconds = this.toSecondsPart().toString().padStart(2, '0')
+        "$hours:$minutes:$seconds"
+    }
+}
