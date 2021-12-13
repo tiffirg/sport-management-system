209db70347178a80bfe48b09d34b7406269c8ca9
nadiaroschina <nadysha.rr@gmail.com>
2021-12-14 00:55:10 +0300

    DistanceCriteria interface updated with getResult

--- src/main/kotlin/ru/emkn/kotlin/sms/classes/DistanceCriteria.kt
+++ src/main/kotlin/ru/emkn/kotlin/sms/classes/DistanceCriteria.kt
@@ -1,5 +1,7 @@
 package ru.emkn.kotlin.sms.classes
 
+import java.time.Duration
+
 enum class DistanceType {
     FIXED, CHOICE
 }
@@ -7,12 +9,17 @@ enum class DistanceType {
 interface DistanceCriteria {
     val distanceType: DistanceType
     fun assertValid(competitorData: CompetitorData) : Boolean
+    fun getResult(competitorData: CompetitorData) : Duration?
     fun getSplit(competitorData: CompetitorData) : List<CheckpointDuration>?
 }
 
 class FixedRace() : DistanceCriteria {
     override val distanceType = DistanceType.FIXED
     override fun assertValid(competitorData: CompetitorData): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun getResult(competitorData: CompetitorData): Duration? {
         TODO("Not yet implemented")
     }
 
@@ -25,6 +32,10 @@ class FixedRace() : DistanceCriteria {
 class ChoiceRace: DistanceCriteria {
     override val distanceType = DistanceType.CHOICE
     override fun assertValid(competitorData: CompetitorData): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun getResult(competitorData: CompetitorData): Duration? {
         TODO("Not yet implemented")
     }
 
